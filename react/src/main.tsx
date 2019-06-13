69bf448d08dea74c1b60a7afdb4699948e96c53d
lian1925 <lian1925@qq.com>
2019-06-13 10:29:56 +0800

    feat(init): init package

---
+++ react/src/main.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import * as ReactDom from 'react-dom'
+
+ReactDom.render(<div>手工系列</div>, document.getElementById("root"))
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+## 技巧
+
+1、让vs code 支持 webpack的alias
+[](https://www.jianshu.com/p/552eac30ddbf)
+
